46d8bc3d58ec986e42f252466f7e529e260026f5
Inamarie84 <123692666+Inamarie84@users.noreply.github.com>
2025-01-20 13:38:21 +0100

    added content to feed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "custom-pink": "#ED5AB4", // Your custom pink
-        "light-custom-pink": "#F9BFD9", // Lighter pink
-        teal: "#4C9F70", // Teal
-        orange: "#FF7F32", // Orange
-        "light-blue": "#A2D9FF", // Light Blue
-        "soft-yellow": "#FFEE58", // Soft Yellow
-        "dark-gray": "#333333", // Dark Gray for text
-        "light-gray": "#F1F1F1", // Light Gray for backgrounds
+        "custom-pink": "#ED5AB4",
+        "light-custom-pink": "#F9BFD9",
+        teal: "#4C9F70",
+        "hover-teal": "#3A7D58",
+        "dark-gray": "#333333",
+        "button-grey": "#1F2937",
+        "light-gray": "#F5F5F5",
       },
       fontFamily: {
         sans: ["Inter", "sans-serif"], // For text
