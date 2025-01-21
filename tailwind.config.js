c9469289a289f7cb2ab759ec4fa468dcead13711
Inamarie84 <123692666+Inamarie84@users.noreply.github.com>
2025-01-21 20:47:52 +0100

    fixing errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./**/*.{html,js}', '!./node_modules/**/*'],
   theme: {
     extend: {
+      backgroundImage: {
+        'custom-bg': "url('/images/backgroundimage.jpg')",
+      },
       colors: {
         'custom-pink': '#ED5AB4',
         'light-custom-pink': '#F9BFD9',
